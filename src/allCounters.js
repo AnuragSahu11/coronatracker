c2cb2781fe2efd70b489dd5776f4b46253324d19
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2020-09-16 10:59:57 +0530

    API portion complete

--- src/allCounters.js
+++ src/allCounters.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Counter from './counter'
 
+
 function AllCounters(){
-    return(
+
+
+    const [data,setData] = useState({})
+    const [loading,setLoading] = useState(true)
+    const [district,setDistrict] = useState('Durg')
+
+    useEffect(() => {
+        fetch('https://api.covid19india.org/state_district_wise.json')
+        .then(response => response.json())
+        .then(coronaData => {
+                const cData = coronaData.Chhattisgarh.districtData
+                setLoading(false)
+                var temporatyData;
+                if( cData.hasOwnProperty(district) ) {
+                    temporatyData = cData[district];
+                    setData(temporatyData);
+                }
+             })
+    },[])
+
+    return( loading? <h1>loading</h1>:
+        
+        (   
         <div>
-            <Counter name='total' />
-            <Counter name='active' />
-            <Counter name='cured' />
-            <Counter name='death' />
+            <Counter name={data.confirmed} />
+            <Counter name={data.active} />
+            <Counter name={data.recovered} />
+            <Counter name={data.deceased} />
             
-        </div>
+        </div>)
     )
 } 
 
